c6eef567df30c8e8767a068fc6b8f671905646a3
Valentin T <92257425+ElFatFat@users.noreply.github.com>
2025-02-03 14:57:45 +0100

    New features

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
